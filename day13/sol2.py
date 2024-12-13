e776a42153f0f403ffbbed3169779249dbcd4fe7
Nignik <maskymilian.olbrys@gmail.com>
2024-12-13 22:31:28 +0100

    day 13

--- day13/sol2.py
+++ day13/sol2.py
@@ -3,6 +3,7 @@ import regex as re
 import sys
 import numpy as np
 import math
+from fractions import Fraction
 
 N = 10000000000000
 
@@ -26,28 +27,38 @@ def load_data(file_name):
       s = data[i + j]
       pattern = r"\d+"
       match = re.findall(pattern, s)
-      x.append(np.array([int(match[0]) + N, int(match[1]) + N]))
-      
+      if s[0] == "P":
+        x.append(np.array([int(match[0]) + N, int(match[1]) + N]))
+      else:
+        x.append(np.array([int(match[0]), int(match[1])]))
+
     res.append(Machine(x[0], x[1], x[2]))
 
   return res
+
+def is_whole(norm, tol=1e-9):
+  return abs(norm - round(norm)) < tol
 
 def sol2(file_name):
   machines = load_data(file_name)
   
   ans = 0 
   for A, B, P in machines:
-    price = math.inf 
+    price = 0
+    denom = A[0]*B[1] - A[1]*B[0]
+    if denom == 0:
+      continue
     
-    diff = P[0] - P[1] 
-    for i in range(101):
-      for j in range(101):
-        pos = A * i + B * j
-        if i * (A[0] - A[1]) + j * (B[0] - B[1]) == diff and np.all(pos * N == P):
-          #print(i, j)
-          price = min(price, 3 * i + j)
-    
-    ans += price if price != math.inf else 0
+    i = Fraction(P[0]*B[1] - P[1]*B[0], denom)
+    j = Fraction(A[0]*P[1] - A[1]*P[0], denom)
+
+    if i.denominator == 1 and j.denominator == 1:
+      i_int = i.numerator
+      j_int = j.numerator
+      price = i_int * 3 + j_int
+      print(i_int, j_int)
+  
+    ans += price
     
   return ans 
   
