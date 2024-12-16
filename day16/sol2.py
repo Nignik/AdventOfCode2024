5bc2769c99a378b6ba1bb08f5aa71d2a6e8c4dda
Nignik <maskymilian.olbrys@gmail.com>
2024-12-16 21:42:55 +0100

    day 16

--- day16/sol2.py
+++ day16/sol2.py
@@ -38,7 +38,6 @@ def sol(file_name):
   q = [start]
   scores = defaultdict(lambda: [float('inf')] * 4)
   scores[(start[0], start[1])] = [1000, 0, 2000, 1000]
-  contributes = defaultdict(lambda: [])
   while len(q) != 0:
     next_step = []
     for pos in q:
@@ -52,19 +51,25 @@ def sol(file_name):
       for d in range(4):
         p = pos + directions[d]
         r, c = p
+        
         new_score = cost[d] + 1
+        new_score_right = new_score + 1000
+        new_score_down = new_score + 2000
+        new_score_left = new_score + 1000
         
-        if new_score == scores[(r, c)][d]:
-          contributes[(r, c, d)].append((pos[0], pos[1]))
-        elif new_score > scores[(r, c)][d]:
-          contributes[(r, c, d)] = [(pos[0], pos[1])]
-          
-        if grid[r][c] == '#' or new_score >= scores[(r, c)][d]:
-          continue
+        if grid[r][c] == '#':
+          continue 
         
-        scores[(r, c)][d] = new_score
-        next_step.append(p)
-      
+        if new_score < scores[(r, c)][d]:
+          scores[(r, c)][d] = new_score
+          next_step.append(p)
+          if new_score_right < scores[(r, c)][(d + 1) % 4]:
+            scores[(r, c)][(d + 1) % 4] = new_score_right
+          if new_score_down < scores[(r, c)][(d + 2) % 4]:
+            scores[(r, c)][(d + 2) % 4] = new_score_down
+          if new_score_left < scores[(r, c)][(d + 3) % 4]:
+            scores[(r, c)][(d + 3) % 4] = new_score_left
+        
     q = next_step
   
   score_grid = [[float('inf')] * len(grid[0]) for _ in range(len(grid))]
@@ -72,33 +77,45 @@ def sol(file_name):
     for c in range(len(grid[0])):
       score_grid[r][c] = min(scores[(r, c)])
   
-  positions = []
+  positions = set()
   vis = defaultdict(bool)
-  q = [(end, 0), (end, 1), (end, 2), (end, 3)]
-  pos = end
-  print(contributes)
+  
+  q = []
+  min_end = min(scores[(end[0], end[1])])
+  for d in range(4):
+    if scores[(end[0], end[1])][d] == min_end:
+      q.append((end, d))
+      
   while len(q) != 0:
     pos, di = q.pop()
-    vis[(pos[0], pos[1])] = True
-    for d in range(4):
-      p = pos + directions[d]
-       
-      if (p[0], p[1]) in contributes[(pos[0], pos[1], (di + 2) % 4)] and not vis[(p[0], p[1])]:
-        q.append((p, d))
-        positions.append(p)
-  
+    # if vis[(pos[0], pos[1])]:
+    #   continue
+    # vis[(pos[0], pos[1])] = True
+    
+    for i in range(1, 4):
+      p = pos + directions[(di + i) % 4]
+      scoreA = scores[(p[0], p[1])][(di + i + 2) % 4]
+      scoreB = scores[(pos[0], pos[1])][di]
+      sub = 1
+      if i == 1 or i == 3:
+        sub += 1000
+      if scoreA == scoreB - sub:
+        q.append((p, (di + i + 2) % 4))
+        positions.add((p[0], p[1]))
+    
   for r in range(len(score_grid)):
     s = []
     for c in range(len(score_grid)):
-      if any(np.array_equal(arr, np.array([r, c])) for arr in positions):
+      if (r, c) in positions:
         s.append(str('O'))
       else:
-        s.append(str('.'))
+        s.append(str(grid[r][c]))
     print("".join(s))
   print()
    
-  print(pd.DataFrame(score_grid))
-  return len(positions)
+  # print(pd.DataFrame(score_grid))
+  return len(positions) + 1
   
-print(f"Star 1:\n test: {sol('test.in')}")
-#print(f" answer: {sol('input.in')}\n")
+print(f"Test: {sol('test.in')}")
+print(f"Other test: {sol('test2.in')}")
+print(f"Answer: {sol('input.in')}\n")
